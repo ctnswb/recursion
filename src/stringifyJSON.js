6b82d582c90da99eceed3ae29f0d0505dc13e27c
ctnswb <xclai.07@gmail.com>
2017-05-04 23:16:55 -0700

    made a plan

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,49 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  // initiate final string stringified = ''
+
+  // if object is a String
+    // stringified = '"obj"'
+
+  // if object is a Boolean
+    // if true, stringified = 'true'
+  // // else stringified = 'false'
+
+  // if object is a Number
+    // stringified = number.toString()
+
+  // if object is an Array
+    // add "[" to stringified
+    // for each value in array
+      // add stringifyJSON(value) to stringified
+      // if its not the last value
+        // add ", " to stringified
+    // add "]" to stringified
+
+  // if object is an Object
+    // add "{" to stringified
+    // for each key value pair
+      // add "key" to stringified
+      // add ":" to stringified
+      // add stringifyJSON(obj[key]) to stringified
+      // if its not the last key/value
+        // add "," to stringified
+    // add "}" to stringified
+
+  // return final string
+
 };
+
+/* tests
+var o = {"x":4 , "y":5};
+var str = '';
+for (var i in o) {
+    str += '"';
+    str += i;
+    str += '"';
+    console.log(str);
+
+}
+*/
